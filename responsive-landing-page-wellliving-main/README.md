0b6e2cf03639bb8ad3bff5f774a68d4a92a2c281
SudaisDeveloper <81598922+SudaisDeveloper@users.noreply.github.com>
2021-06-13 13:53:08 +0300

    Delete responsive-landing-page-wellliving-main directory

--- responsive-landing-page-wellliving-main/README.md
+++
@@ -1,8 +0,0 @@
-
-# Responsive Landing Page WellLiving 🏡
-
-
-Thanks,
-[Sudais Coder](https://www.youtube.com/c/SudaisCoder)
-
-![preview](https://github.com/SudaisDeveloper/WellLiving/blob/731765f3becfb1eec6767f0918601d0707871dab/responsive-landing-page-wellliving-main/preview.jpg)
--- responsive-landing-page-wellliving-main/js/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".hamb");
-const navList = document.querySelector(".nav-list");
-const links = document.querySelector(".nav-list li");
-
-hamburger.addEventListener("click", function() {
-	hamburger.classList.toggle("click");
-	navList.classList.toggle("open");
-})
