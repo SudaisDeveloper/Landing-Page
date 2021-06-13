83e02481e1f3db7eb8dee51cba6d842dbe7cc3a8
SudaisDeveloper <81598922+SudaisDeveloper@users.noreply.github.com>
2021-06-13 13:53:36 +0300

    Delete Responsive-landing-page-color directory

--- Responsive-landing-page-color/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamb");
-const navLinks = document.querySelector(".nav-list");
-
-hamburger.addEventListener("click", function() {
-	navLinks.classList.toggle("open");
-})
