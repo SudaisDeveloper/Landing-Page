8d0fa903a50139f820611433a2f06e14f65730ad
SudaisDeveloper <81598922+SudaisDeveloper@users.noreply.github.com>
2021-06-13 13:54:29 +0300

    Delete Responsive-landing-page-prev directory

--- Responsive-landing-page-prev/js/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamb");
-const navBox = document.querySelector(".nav_box");
-
-hamburger.addEventListener("click", function() {
-  navBox.classList.toggle("check");
-  hamburger.classList.toggle("click");
-})
