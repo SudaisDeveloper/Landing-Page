7b06e98957ad88babb53aabc7d52c457fecfa250
SudaisDeveloper <81598922+SudaisDeveloper@users.noreply.github.com>
2021-06-13 13:52:54 +0300

    Delete Responsive-landing-page-chump directory

--- Responsive-landing-page-chump/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamb");
-const nav = document.querySelector(".nav_box");
-
-hamburger.addEventListener("click", function() {
-  nav.classList.toggle("check");
-})
