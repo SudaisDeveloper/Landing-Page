3589534835daad94c213c42ebb58d27c0951b67a
SudaisDeveloper <81598922+SudaisDeveloper@users.noreply.github.com>
2021-06-13 13:55:48 +0300

    Delete Responsive-landing-page-yellowblack directory

--- Responsive-landing-page-yellowblack/README.md
+++
@@ -1,7 +0,0 @@
-# Responsive Landing Page YellowBlack
-
-
-Thanks,
-[Sudais Coder](https://www.youtube.com/c/SudaisCoder)
-
-![preview](https://user-images.githubusercontent.com/76812554/111670471-123ae200-8842-11eb-923a-5cf6abf5a317.jpg)
--- Responsive-landing-page-yellowblack/js/script.js
+++
@@ -1,7 +0,0 @@
-const hamb = document.querySelector(".hamb");
-const navList = document.querySelector(".menu-overlay");
-
-hamb.addEventListener("click", function() {
-	navList.classList.toggle("active");
-	hamb.classList.toggle("click");
-})
